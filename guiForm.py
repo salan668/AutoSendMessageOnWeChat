c3aee0c4198d5dcb0b71c5cd5b3e92673de92025
Y.S XPS-9570 <songyangmri@gmail.com>
2020-12-31 16:44:49 +0800

    界面增加了倒计时显示

--- guiForm.py
+++ guiForm.py
@@ -14,6 +14,7 @@ import pyperclip
 import pyautogui as ag
 from PyQt5.QtWidgets import *
 from PyQt5 import QtGui
+from PyQt5.QtCore import QThread, pyqtSignal, QTimer
 from PIL import ImageQt, Image
 
 from WechatProcess import Contact
@@ -32,10 +33,25 @@ def ConvertByte2Image(code, picture_name):
         f.write(base64.b64decode(code))
 
 
+class ClockCount(QThread):
+    signal = pyqtSignal(str)
+
+    def __init__(self):
+        super().__init__()
+
+    def run(self):
+        pass
+
+
 class GuiForm(QWidget, Ui_Form):
     def __init__(self, parent=None):
+        # self.thread = ClockCount()
         super(GuiForm, self).__init__(parent)
         self.setupUi(self)
+
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.ClockCount)
+        self.remain_time = 10
 
         ConvertByte2Image(ali, 'ali.png')
         self.label_5.setPixmap(QtGui.QPixmap('ali.png'))
@@ -57,40 +73,17 @@ class GuiForm(QWidget, Ui_Form):
             self.contact.LoadConfig(file_name)
             self.lineEditContact.setText(file_name)
 
+    def ClockCount(self):
+        if self.remain_time > 0:
+            self.remain_time -= 1
+        else:
+            self.timer.stop()
+        self.labelRemainTime.setText('激活微信剩余时间：\n{}s'.format(self.remain_time))
+
     def Run(self):
         ag.PAUSE = 1
 
-        # top_windows = []
-        # win32gui.EnumWindows(window_enumeration_handler, top_windows)
-        # for (id, title) in sorted(top_windows, reverse=False):
-        #     if win32gui.IsWindowVisible(id) and title and ('Program Manager' not in title) and \
-        #             ('Microsoft' not in title) and ('Window' not in title):
-        #         win32gui.ShowWindow(id, win32con.SW_MINIMIZE)
-        #
-        # has_window = False
-        # for (id, title) in sorted(top_windows, reverse=False):
-        #     if '微信' in title and win32gui.IsWindowVisible(id):
-        #         print(win32gui.GetWindowText(id))
-        #         win32gui.ShowWindow(id, win32con.SW_NORMAL)
-        #         win32gui.SetForegroundWindow(id)
-        #         has_window = True
-        #         break
-        #
-        # if not has_window:
-        #     for (id, title) in sorted(top_windows, reverse=False):
-        #         if 'WeChat' in title and win32gui.IsWindowVisible(id):
-        #             print(win32gui.GetWindowText(id))
-        #             win32gui.ShowWindow(id, win32con.SW_NORMAL)
-        #             win32gui.SetForegroundWindow(id)
-        #             has_window = True
-        #             break
-        #
-        # if not has_window:
-        #     win32gui.MessageBox(id, 'No 微信 or WeChat', '', (win32con.MB_OK | win32con.MB_ICONINFORMATION))
-        #     return None
-
-
-        time.sleep(5)
+        self.timer.start(1000)
 
         for person, content in self.contact.Browse():
             time.sleep(1)
@@ -107,7 +100,6 @@ class GuiForm(QWidget, Ui_Form):
             ag.keyDown('enter')
 
         QMessageBox.about(self, 'Done', 'Done')
-        # win32gui.MessageBox(self, 'Done', '', (win32con.MB_OK | win32con.MB_ICONINFORMATION))
 
 
 if __name__ == '__main__':
